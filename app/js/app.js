9565e0379e19d1785300f66d96ddaf80e15570e6
Pawel Nosal <pawel.nosal@ocado.com>
2017-07-26 08:53:55 +0200

    clean project

--- app/js/app.js
+++ app/js/app.js
@@ -1,36 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../scss/application.scss';
-import UserTable from './users/UserTable'
 
-
-
-const users = [
-    {
-        'id': 1,
-        'name': 'Pawel',
-        'email': 'pawel4@test.pl',
-        'year': 1986
-    },
-    {
-        'id': 2,
-        'name': 'Pawel2',
-        'email': 'pawel6@test.pl',
-        'year': 1986
-    },
-    {
-        'id': 3,
-        'name': 'Pawel3',
-        'email': 'pawel1@test.pl',
-        'year': 1986
-    },
-    {
-        'id': 4,
-        'name': 'Pawel4',
-        'email': 'pawel3@test.pl',
-        'year': 1986
-    }
-];
-
-
-ReactDOM.render(<UserTable users={users}/>, document.getElementById('app'));
+ReactDOM.render(<p>Hello world</p>, document.getElementById('app'));
